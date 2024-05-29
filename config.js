1ebdf6dcf241808c4bb4b7f642fa1663305196ab
bobwatcherx <bobwatcherx@gmail.com>
2024-05-29 13:58:00 +0700

    dqdwq

---
+++ config.js
@@ -0,0 +1,20 @@
+let key_api = "219725bbkborbourrp2cd4"
+let cors_any   = "https://bobwatcherx-corsanywhere3.hf.space"
+
+let frame_domain = "https://d000d.com"
+
+let shortlink_domain  = "https://cuty.io/quick?token="
+let shortlink_token  = "3c2f8445e662326c2ebcd8d60"
+let shortlink_tujuan  = "&url="
+
+
+let directlink_url = "https://biodegradablegrand.com/bM3RVQ0WP.3ip/v/b/mJVOJQZMDS0V1-MjjzI/zlOhDfAU2mLlTzUjykMFjcMM4KMsDacf"
+
+function direct_click(code){
+	let random_pilih =  Math.floor(Math.random() * 2) + 1;
+	if(random_pilih === 2){
+		window.open(directlink_url,'_blank')
+	}else{
+		window.location.href = `/p.html?id=${code}`
+	}
+}
