e7b519892d8c5524736fedbbdb3f189e01ea94ad
joesandiroz2 <joesandiroz2@gmail.com>
2024-10-23 10:54:40 +0700

    #12

--- config.js
+++ config.js
@@ -8,6 +8,8 @@ let shortlink_token  = "3c2f8445e662326c2ebcd8d60"
 let shortlink_tujuan  = "&url="
 
 
+let midtrans_payment = "https://darenx-popdownload.hf.space/create"
+
 let directlink_url = "https://darkbananas.com/in/p/?spot_id=830736&cat=25&sub_id=1246634411"
 
 
@@ -29,6 +31,7 @@ function direct_click(code){
             timer:1000
           });
     }
+
 
 
 
